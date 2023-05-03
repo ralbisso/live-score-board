89c88f3b9ac77684293ddcd57da401ed846c8b8f
Romain Albisson <albisson.romain@gmail.com>
2023-05-03 10:12:04 +0200

    Add live soccer score board App

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,176 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, watch } from "vue"
+
+const homeTeam = ref("")
+const awayTeam = ref("")
+const games = ref([])
+const finishedGames = ref([])
+
+const isDisabled = () =>
+  !homeTeam.value ||
+  !awayTeam.value ||
+  homeTeam.value === awayTeam.value ||
+  isOneTeamPlaying(homeTeam.value, awayTeam.value)
+
+const isOneTeamPlaying = (team1, team2) => {
+  return isPlaying(team1) || isPlaying(team2)
+}
+
+const isPlaying = (team) => {
+  return games.value.filter(t => t.homeTeam === team || t.awayTeam === team).length > 0
+}
+
+const addGame = () => {
+  games.value.push({
+    id: Date.now(),
+    homeTeam: homeTeam.value,
+    homeScore: 0,
+    awayTeam: awayTeam.value,
+    awayScore: 0
+  })
+  homeTeam.value = ""
+  awayTeam.value = ""
+}
+
+const finishGame = (game) => {
+  const finishedGame = games.value.splice(game, 1)[0]
+  finishedGame.finishedAt = getFormattedDate(new Date())
+  finishedGames.value.push(finishedGame)
+}
+
+const getFormattedDate = (date) => {
+  var day = addLeadingZeroes(date.getDay() + "")
+  var month = addLeadingZeroes((date.getMonth() + 1) + "")
+  var year = date.getFullYear()
+  var hour = addLeadingZeroes(date.getHours() + "")
+  var minutes = addLeadingZeroes(date.getMinutes() + "")
+  var seconds = addLeadingZeroes(date.getSeconds() + "")
+  return day + '/' + month + '/' + year + ' ' + hour + ':' + minutes + ':' + seconds
+}
+
+const addLeadingZeroes = (param) => {
+  if (param.length == 1) {
+    param = "0" + param;
+  }
+  return param;
+}
+
+watch(games.value, () => {
+  games.value.sort((game1, game2) => {
+    const totalScoreGame1 = getTotalScore(game1)
+    const totalScoreGame2 = getTotalScore(game2)
+    return totalScoreGame2 - totalScoreGame1
+  })
+})
+
+const getTotalScore = (game) => game.homeScore + game.awayScore
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="container">
+    <h1>Welcome to the live score board !</h1>
+    <div class="new-game">
+      <fieldset>
+        <legend>Please add here your new game</legend>
+        <div>
+          <label for="homeTeam">Team 1:</label>
+          <input id="homeTeam" v-model.trim="homeTeam" placeholder="Add home team here">
+        </div>
+        <div>
+          <label for="awayTeam">Team 2:</label>
+          <input id="awayTeam" v-model.trim="awayTeam" placeholder="Add away team there">
+        </div>
+        <button id="newGameButton" :disabled=isDisabled() @click="addGame()">Add game</button>
+        <div class="constraints">
+          <ul>
+            <li :class="{ error: !homeTeam }">Home team should not be null</li>
+            <li :class="{ error: !awayTeam }">Away team should not be null</li>
+            <li :class="{ error: homeTeam === awayTeam }">Home and away team should not be the same</li>
+            <li :class="{ error: isOneTeamPlaying(homeTeam, awayTeam) }">Home and/or away team should not be already
+              playing
+            </li>
+          </ul>
+        </div>
+      </fieldset>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="games-container">
+      <div class="games">
+        <table>
+          <caption>Ongoing games</caption>
+          <tr v-for="(game, index) in games" :key="game.id">
+            <td><button @click="game.homeScore++">Goal</button></td>
+            <td>{{ game.homeTeam }}</td>
+            <td>{{ game.homeScore }}</td>
+            <td>-</td>
+            <td>{{ game.awayScore }}</td>
+            <td>{{ game.awayTeam }}</td>
+            <td><button @click="game.awayScore++">Goal</button></td>
+            <td><button @click="finishGame(index)">Finish this game</button></td>
+          </tr>
+        </table>
+      </div>
+      <div class="games">
+        <table>
+          <caption>Finished games</caption>
+          <tbody>
+            <tr v-for="game in finishedGames" :key="game.id">
+              <td>{{ game.homeTeam }}</td>
+              <td>{{ game.homeScore }}</td>
+              <td>-</td>
+              <td>{{ game.awayScore }}</td>
+              <td>{{ game.awayTeam }}</td>
+              <td>{{ game.finishedAt }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+.container {
+  font-family: monospace;
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.new-game {
+  margin: auto;
+  width: 40%;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+input,
+table {
+  margin: 10px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+caption {
+  border: 2px solid;
+  font-size: 1.2rem;
+  font-weight: 900;
 }
 
-nav a:first-of-type {
-  border: 0;
+button {
+  font-size: 1.1rem;
+  font-family: inherit;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+li {
+  color: forestgreen;
+  text-align: left;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+table {
+  min-width: 500px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.error {
+  color: red;
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.games-container {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
